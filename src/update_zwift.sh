838db3c1f5786702dae43d551dcf43ee1ae456c8
Glenn Van Loon <glenn.vanloon@gmail.com>
2025-09-15 10:09:30 +0200

    Install vc++2015 redistributable

--- src/update_zwift.sh
+++ src/update_zwift.sh
@@ -110,6 +110,9 @@ if [[ -z "$(ls -A .)" ]]; then # is directory empty?
     # install dotnet48 for zwift
     winetricks -q dotnet48
 
+    # install vc++ 2015 redistributable for zwift
+    winetricks -q vcrun2015
+
     # Install D3D Compiler to allow Vulkan Shaders.
     winetricks d3dcompiler_47
 
