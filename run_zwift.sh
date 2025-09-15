afe2b1cd9bd5fd47462f0b733e59660cb000a25d
Glenn Van Loon <glenn.vanloon@gmail.com>
2025-09-15 11:02:02 +0200

    disable wine logging

--- run_zwift.sh
+++ run_zwift.sh
@@ -34,9 +34,9 @@ LAUNCHER_PID=$((16#$LAUNCHER_PID_HEX))
 if [[ ! -z "$ZWIFT_USERNAME" ]] && [[ ! -z "$ZWIFT_PASSWORD" ]];
 then
     echo "authenticating with zwift..."
-    WINEDEBUG=+wgl wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe --token=$(zwift-auth) &> /home/user/.wine/drive_c/users/user/Documents/Zwift/Logs/wine.log
+    wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe --token=$(zwift-auth)
 else
-    WINEDEBUG=+wgl wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe &> /home/user/.wine/drive_c/users/user/Documents/Zwift/Logs/wine.log
+    wine start /exec /bin/runfromprocess-rs.exe $LAUNCHER_PID ZwiftApp.exe
 fi
 
 sleep 3
