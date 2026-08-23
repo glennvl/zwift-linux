6a05431604935a81e8e548d0be72be49aa7f4828
Glenn Van Loon <glenn.vanloon@gmail.com>
2026-08-23 14:19:01 +0200

    Remove docker in docker workaround
    It is fixed in https://github.com/devcontainers/features/pull/1666.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! docker info > /dev/null 2>&1; then
-    sudo update-alternatives --set iptables /usr/sbin/iptables-nft
-fi
-
 npm install -g markdownlint-cli2 cspell
 
 gem update --system
